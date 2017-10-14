d7f29f0735a1d7816e6366c7b4e1abb1e0e5ad5a
Johan Bloemberg <github@ijohan.nl>
2017-10-14 12:12:00 +0200

    Fix bootstrap dependencies.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -30,7 +30,7 @@ curl -s "http://apt.puppetlabs.com/puppetlabs-release-pc1-${release}.deb" \
   -o "puppetlabs-release-pc1-${release}.deb"
 dpkg -i "puppetlabs-release-pc1-${release}.deb"
 apt-get -q update
-apt-get install -yqq puppet rsync apt-transport-https
+apt-get install -yqq puppet rsync apt-transport-https git
 gem install hiera-eyaml deep_merge hiera highline librarian-puppet
 rm -f "puppetlabs-release-pc1-${release}.deb"
 
