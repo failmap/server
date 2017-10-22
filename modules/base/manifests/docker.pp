3e29f7d0b489c479f4815b23ef97ea4bb7e68914
Johan Bloemberg <github@ijohan.nl>
2017-10-22 13:44:25 +0200

    docker memory accounting

--- modules/base/manifests/docker.pp
+++ modules/base/manifests/docker.pp
@@ -8,8 +8,22 @@ class base::docker {
     image   => 'gliderlabs/registrator:latest',
     net     => host,
     volumes => [
-      '/var/run/docker.sock:/tmp/docker.sock',
+    '/var/run/docker.sock:/tmp/docker.sock',
     ],
     command => '-internal consul://localhost:8500',
+  }
+
+  # enable memory accounting for `docker stats`
+  # http://awhitehatter.me/debian-jessie-wdocker/
+  if ::lsbdistcodename == 'jessie' {
+    file_line {'docker memory stats':
+      line    => 'GRUB_CMDLINE_LINUX_DEFAULT="quiet cgroup_enable=memory swapaccount=1"',
+      match   => 'GRUB_CMDLINE_LINUX_DEFAULT',
+      replace => true,
+      path    => '/etc/default/grub',
+    } ~> exec {'update grub':
+      command     => '/usr/sbin/update-grub',
+      refreshonly => true,
+    }
   }
 }
