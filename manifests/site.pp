52ff21f39483a38d67197e9dcb674f42cf78d8a0
Johan Bloemberg <github@ijohan.nl>
2018-01-24 19:14:09 +0100

    Adapt to version 5 hiera config.
    - Eyaml was never used (and not intended to), so it could be dropped
    - deep merge behavior should only be required for `classes` but is not tested completely.

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,5 +8,5 @@ notice("fqdn: ${::fqdn}, env: ${env}")
 
 # no node definitions here, get all config from hiera
 # https://docs.puppet.com/hiera/3.1/puppet.html#assigning-classes-to-nodes-with-hiera-hierainclude
-hiera_include('classes', [])
+lookup('classes', {merge => unique}).include
 
