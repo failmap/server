174b539b6cc03ee1b3918ea6211cdd109e42ce35
Johan Bloemberg <github@ijohan.nl>
2018-02-15 21:39:22 +0100

    mgmt endeavour

---
+++ modules/mgmt/manifests/init.pp
@@ -0,0 +1,16 @@
+# install and manage mgmt config
+class mgmt {
+  file { '/usr/local/bin/mgmt':
+    source => 'puppet:///modules/mgmt/mgmt-linux-amd64',
+    mode   => '0755',
+  }
+
+  file { '/etc/systemd/system/mgmt.service':
+    source => 'puppet:///modules/mgmt/mgmt.service',
+    mode   => '0644',
+  }
+  ~> service {'mgmt':
+    ensure => running,
+    enable => true,
+  }
+}
