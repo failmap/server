9b3a1f1531485f97ac9859cdd6525413e7c7083d
Johan Bloemberg <github@ijohan.nl>
2018-08-14 21:06:20 +0200

    os check

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 
 set -e 
 
+if ! test "$(whoami)" == "root";then
+  echo "Must run as root!"
+  exit 1
+fi
+
+if ! grep "Debian GNU/Linux 8" /etc/os-release;then
+  echo "OS must be Debian 8"
+  exit 1
+fi
+
 apt update -q
-apt install git -yq
+apt install git curl -yqq
 mkdir -p /opt/failmap/server/
 git clone https://gitlab.com/failmap/server.git /opt/failmap/server/
 /opt/failmap/server/scripts/bootstrap.sh
