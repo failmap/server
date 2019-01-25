7ee7b18648306e2fc4af9dd7cff5aef56723195e
Elger Jonker <elger.jonker@gmail.com>
2019-01-25 10:08:11 +0000

    Removed hyper, added a claim_proxy worker for another series of potentially blocking/deadlocking tasks.

--- code/puppet/modules/apps/manifests/failmap/worker.pp
+++ code/puppet/modules/apps/manifests/failmap/worker.pp
@@ -45,7 +45,7 @@ class apps::failmap::worker (
 
   # Several worker instances are created, one for generic administrative tasks (storage),
   # one for rate limited qualys scans, one for v6, v4 and one for both
-  $worker_roles = ['storage', 'qualys', 'v6_internet', 'v4_internet', 'all_internet', 'hyper']
+  $worker_roles = ['storage', 'qualys', 'v6_internet', 'v4_internet', 'all_internet', 'claim_proxy']
   $worker_roles.each | $role | {
     if $workers_configuration[$role] {
       $worker_args = join($workers_configuration[$role], ' ')
