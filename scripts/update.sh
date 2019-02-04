f1bb0e972cb4bd6154cb15bbf612cfbb1d2a53f6
Johan Bloemberg <github@ijohan.nl>
2019-02-04 11:20:52 +0100

    Update modules, pass arguments to apply

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,12 +2,12 @@
 
 # pull in upstream changes and apply new configuration
 
-set -e
-
 if ! test "$(whoami)" == "root";then
   echo "Must run as root or using sudo!"
   exit 1
 fi
+
+set -ex
 
 cd /opt/failmap/server
 # pull in latest changed from upstream
@@ -17,5 +17,8 @@ git remote update
 branch=$(git rev-parse --abbrev-ref HEAD)
 git reset --hard "origin/$branch"
 
+# make sure puppet correct modules are installed
+(cd code/puppet/; librarian-puppet install)
+
 # apply changes
-/opt/failmap/server/scripts/apply.sh
+/opt/failmap/server/scripts/apply.sh "$@"
